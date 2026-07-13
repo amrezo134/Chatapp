1e726d74fb0dea905569ce36927eab8c97a1fd64
amrezo134 <amrezo134@gmail.com>
2026-07-14 01:40:24 +0300

    Add senderName property to GroupMessage data class

--- ChatApp/app/src/main/java/com/creatix/chatapp/data/GroupMessage.kt
+++ ChatApp/app/src/main/java/com/creatix/chatapp/data/GroupMessage.kt
@@ -3,6 +3,7 @@ package com.creatix.chatapp.data
 data class GroupMessage(
     val id: String = "",
     val senderId: String = "",
+    val senderName: String = "",
     val receiverId: String = "",
     val text: String = "",
     val timestamp: Long = System.currentTimeMillis(),
